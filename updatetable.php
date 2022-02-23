cf144b89bcb302873a5fde0097ac93d6030c07ed
SonamMishra1998 <60139077+SonamMishra1998@users.noreply.github.com>
2022-02-24 01:16:14 +0530

    Update updatetable.php

--- updatetable.php
+++ updatetable.php
@@ -1,17 +1,70 @@
-POST / HTTP/1.1
-Host: dynamodb.<region>.<domain>;
-Accept-Encoding: identity
-Content-Length: <PayloadSizeBytes>
-User-Agent: <UserAgentString>
-Content-Type: application/x-amz-json-1.0
-Authorization: AWS4-HMAC-SHA256 Credential=<Credential>, SignedHeaders=<Headers>, Signature=<Signature>
-X-Amz-Date: <Date>
-X-Amz-Target: DynamoDB_20120810.UpdateTable
+<? php
+    
+/**
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+*/
 
-{
-    "TableName": "Thread",
-    "ProvisionedThroughput": {
-        "ReadCapacityUnits": 10,
-        "WriteCapacityUnits": 10
+require 'vendor/autoload.php';
+
+date_default_timezone_set('UTC');
+
+use Aws\DynamoDb\Exception\DynamoDbException;
+use Aws\DynamoDb\Marshaler;
+
+$sdk = new Aws\Sdk([
+    
+    'region'   => 'ap-southeast-1',
+    'version'  => 'latest'
+]);
+
+$dynamodb = $sdk->createDynamoDb();
+$marshaler = new Marshaler();
+
+$tableName = 'Movies';
+
+$movies = json_decode(file_get_contents('moviedata.json'), true);
+
+foreach ($movies as $movie) {
+
+    $year = $movie['year']; 
+    $title = $movie['title'];
+    $info = $movie['info'];
+
+    $json = json_encode([
+        'year' => $year,
+        'title' => $title,
+        'info' => $info
+    ]);
+
+    $params = [
+        'TableName' => $tableName,
+        'Item' => $marshaler->marshalJson($json)
+    ];
+
+    try {
+        $result = $dynamodb->putItem($params);
+        echo "Added movie: " . $movie['year'] . " " . $movie['title'] . "\n";
+    } catch (DynamoDbException $e) {
+        echo "Unable to add movie:\n";
+        echo $e->getMessage() . "\n";
+        break;
     }
+
 }
+
+
+
+
+    
+    
+    ?>
