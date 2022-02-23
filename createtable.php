e5430ed281d25f7cea88873c946a0d0e61b3c9ab
SonamMishra1998 <60139077+SonamMishra1998@users.noreply.github.com>
2022-02-24 00:07:03 +0530

    Create createtable.php

---
+++ createtable.php
@@ -0,0 +1,132 @@
+POST / HTTP/1.1
+Host: dynamodb.<region>.<domain>;
+Accept-Encoding: identity
+Content-Length: <PayloadSizeBytes>
+User-Agent: <UserAgentString>
+Content-Type: application/x-amz-json-1.0
+Authorization: AWS4-HMAC-SHA256 Credential=<Credential>, SignedHeaders=<Headers>, Signature=<Signature>
+X-Amz-Date: <Date>
+X-Amz-Target: DynamoDB_20120810.CreateTable
+
+{
+    "AttributeDefinitions": [
+        {
+            "AttributeName": "ForumName",
+            "AttributeType": "S"
+        },
+        {
+            "AttributeName": "Subject",
+            "AttributeType": "S"
+        },
+        {
+            "AttributeName": "LastPostDateTime",
+            "AttributeType": "S"
+        }
+    ],
+    "TableName": "Thread",
+    "KeySchema": [
+        {
+            "AttributeName": "ForumName",
+            "KeyType": "HASH"
+        },
+        {
+            "AttributeName": "Subject",
+            "KeyType": "RANGE"
+        }
+    ],
+    "LocalSecondaryIndexes": [
+        {
+            "IndexName": "LastPostIndex",
+            "KeySchema": [
+                {
+                    "AttributeName": "ForumName",
+                    "KeyType": "HASH"
+                },
+                {
+                    "AttributeName": "LastPostDateTime",
+                    "KeyType": "RANGE"
+                }
+            ],
+            "Projection": {
+                "ProjectionType": "KEYS_ONLY"
+            }
+        }
+    ],
+    "ProvisionedThroughput": {
+        "ReadCapacityUnits": 5,
+        "WriteCapacityUnits": 5
+    },
+    "Tags": [ 
+      { 
+         "Key": "Owner",
+         "Value": "BlueTeam"
+      }
+   ]
+}
+Sample Response
+HTTP/1.1 200 OK
+x-amzn-RequestId: <RequestId>
+x-amz-crc32: <Checksum>
+Content-Type: application/x-amz-json-1.0
+Content-Length: <PayloadSizeBytes>
+Date: <Date>
+ {
+    "TableDescription": {
+        "TableArn": "arn:aws:dynamodb:us-west-2:123456789012:table/Thread",
+        "AttributeDefinitions": [
+            {
+                "AttributeName": "ForumName",
+                "AttributeType": "S"
+            },
+            {
+                "AttributeName": "LastPostDateTime",
+                "AttributeType": "S"
+            },
+            {
+                "AttributeName": "Subject",
+                "AttributeType": "S"
+            }
+        ],
+        "CreationDateTime": 1.36372808007E9,
+        "ItemCount": 0,
+        "KeySchema": [
+            {
+                "AttributeName": "ForumName",
+                "KeyType": "HASH"
+            },
+            {
+                "AttributeName": "Subject",
+                "KeyType": "RANGE"
+            }
+        ],
+        "LocalSecondaryIndexes": [
+            {
+                "IndexArn": "arn:aws:dynamodb:us-west-2:123456789012:table/Thread/index/LastPostIndex",
+                "IndexName": "LastPostIndex",
+                "IndexSizeBytes": 0,
+                "ItemCount": 0,
+                "KeySchema": [
+                    {
+                        "AttributeName": "ForumName",
+                        "KeyType": "HASH"
+                    },
+                    {
+                        "AttributeName": "LastPostDateTime",
+                        "KeyType": "RANGE"
+                    }
+                ],
+                "Projection": {
+                    "ProjectionType": "KEYS_ONLY"
+                }
+            }
+        ],
+        "ProvisionedThroughput": {
+            "NumberOfDecreasesToday": 0,
+            "ReadCapacityUnits": 5,
+            "WriteCapacityUnits": 5
+        },
+        "TableName": "Thread",
+        "TableSizeBytes": 0,
+        "TableStatus": "CREATING"
+    }
+}
